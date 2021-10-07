7c537161deca3a95f4403da94f424d7a6580d494
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-10-07 13:08:39 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -14,7 +14,6 @@ public class HelloWorld{
 	
      
 }
-
 class MyCalculator {
 	void divide(int num, int den) {
 		System.out.println("\tBegin divide()");
@@ -22,13 +21,33 @@ class MyCalculator {
 		System.out.println("\tden : "+den);
 		System.out.println("\tDividing "+num+" by "+den);
 		
-		if(den!=0) {
+//Java Virtual machine [ virtual processor ] -> physical processor 
+		
+		try { 
+			//business logic
+			int division = num / den; // 100 / 0
+							//ArithmeticException ae = new ArithmeticException("/ by zero");
+							//throw ae;
+			
+			System.out.println("\tdiv : "+division);
+		}
+		catch(ArithmeticException ae) {
+			//error handling logic
+			System.out.println("\tCannot divide "+num+ " by "+den);
+		}
+
+		
+		/*if(den!=0) { 
+			//business logic
 			int division = num / den;
 			System.out.println("\tdiv : "+division);
 		}
 		else {
+			//error handling logic
 			System.out.println("\tCannot divide "+num+ " by "+den);
 		}
+		*/
+		
 		
 		System.out.println("\tEnd divide()");
 		System.out.println("\t----------------");
