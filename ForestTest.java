3c563bb3a8528b158d60fcb09c5e83bdea4aff20
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-10-08 17:36:37 +0530

    Add files via upload

---
+++ ForestTest.java
@@ -0,0 +1,30 @@
+class Tiger {
+}
+
+class Lion {
+}
+
+class SavingsAccount {
+}
+
+class CurrentAccount {
+}
+
+class Flight {
+}
+class CommercialFlight {
+}
+
+class Habit {
+}
+class NeuralHabit {
+}
+class Room {
+}
+class AcRoom {
+}
+class Suite {
+}
+
+
+
