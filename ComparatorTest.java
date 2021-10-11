734fd62873f6860c028931d3edd135cea43dc8b4
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-10-11 11:23:28 +0530

    Add files via upload

--- ComparatorTest.java
+++ ComparatorTest.java
@@ -54,24 +54,105 @@ public class ComparatorTest {
 		jukeBox.add(s4);	jukeBox.add(s5);		
 		
 		//the for each loop
-		for(Song theSong : jukeBox) { // no iterator required
-			System.out.println("Song : "+theSong);
-		}
+		//for(Song theSong : jukeBox) { // no iterator required
+		//	System.out.println("Song : "+theSong);
+		//}
 		
 		//Sorts the specified list into ascending order, 
 		//according to the natural ordering of its elements. 
 		//All elements in the list must implement the Comparable interface.
-		System.out.println("Sorting...");
-		Collections.sort(jukeBox);
-		System.out.println("Sorted...");
+		//System.out.println("Sorting...");
+		//Collections.sort(jukeBox); // wlll go for Comparable
+		//System.out.println("Sorted...");
 		
-		for(Song theSong : jukeBox) { // no iterator required
-			System.out.println("Song : "+theSong);
-		}
+		//for(Song theSong : jukeBox) { // no iterator required
+		//	System.out.println("Song : "+theSong);
+		//}
+		
 		System.out.println("--now lets sort it based on other columns too--");
+		
+		int sortChoice=0;
+		
+		Comparator<Song> cmp = null;
+
+		do {
+			System.out.println("Sort by ");
+			System.out.println("-----------");
+			System.out.println("1. Title");
+			System.out.println("2. Album");
+			System.out.println("3. Artist");
+			System.out.println("4. Year");
+			System.out.println("5. Exit");
+			System.out.println("-----------");
+			System.out.println("Sort Choice :  ");
+			Scanner scan6 = new Scanner(System.in);
+			sortChoice = scan6.nextInt();
+			switch(sortChoice) {
+				case 1: cmp = new TitleComparator(); break;
+				case 2: cmp = new AlbumComparator(); break;
+				case 3: cmp = new ArtistComparator(); break;
+				case 4: cmp = new YearComparator(); break;
+				case 5: System.out.println("Exiting...");
+			
+			}
+			Collections.sort(jukeBox, cmp ); //going with Comparator 
+			for(Song theSong : jukeBox) { // no iterator required
+				System.out.println("Song : "+theSong);
+			}
+			System.out.println("====================");
+		} while(sortChoice!=5);
 		
 	}
 }
+
+class YearComparator implements Comparator<Song> {
+	public int compare(Song x, Song y) {
+		System.out.println("YearComparator : comparing "+x.getYear()+" with "+y.getYear());
+		return Integer.compare(x.getYear(), y.getYear());
+	}
+}
+
+class ArtistComparator implements Comparator<Song> {
+	public int compare(Song x, Song y) {
+		System.out.println("AristComparator : comparing "+x.getArtist()+" with "+y.getArtist());
+		return x.getArtist()    .compareTo(    y.getArtist()     );
+
+	}
+}
+class AlbumComparator implements Comparator<Song> {
+	public int compare(Song x, Song y) {
+		System.out.println("AlbumComparator : comparing "+x.getAlbum()+" with "+y.getAlbum());
+		return x.getAlbum()    .compareTo(    y.getAlbum()     );
+
+	}
+}
+class TitleComparator implements Comparator<Song> {
+	public int compare(Song x, Song y) {
+		System.out.println("TitleComparator : comparing "+x.getTitle()+" with "+y.getTitle());
+		return x.getTitle()    .compareTo(    y.getTitle()     );
+
+	}
+}
+
+/*
+ *   reality shows
+ *   			Comparable
+ *   			|				|			|
+ *   			dancer1 ,		dancer2,   dancer3 .....
+ * 
+ * 
+ * 
+ * 			Energy	Steps	Theme <-- comparators 
+ * 			Judge1  Judge2	Judge3
+ * 
+ * 		Mirror <--> She (rita) | earRings, necklace, makeup .....
+ * 
+ * 		Party <-- (rita) | julie | reema | sita | 
+ * 
+ * 	   Theme  dress		energy
+ * 		man1  woman2  oldman3 <-- comparators
+ * 		
+ */
 
 //annotation  - 2 days for REST API
 
