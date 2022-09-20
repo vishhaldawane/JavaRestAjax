51060cb69bbec335c32fdae6e61b7365be0f0293
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-09-20 09:35:31 +0530

    Add files via upload

---
+++ calculator.sh
@@ -0,0 +1,16 @@
+
+
+	Enter number1 : 50 <-|
+
+	Enter number2 : 40 <-|
+
+	What you want to do ? + <-|  ( +,-,*,/ is the input )
+
+	Answer is 90
+
+	if [ $myinput = '+' ]
+	then
+		echo answer is $((num1+num2))
+
+
+
